955e63faaa1bfdc487db91b40e2b80b8e265fc53
Keeghan <Keeghan@DESKTOP-A43BJ26.local>
2020-11-24 16:47:48 -0800

    Finished out remainder of the lab; all branches tested.

--- src/main/java/csc131/junit/GiftCardTest.java
+++ src/main/java/csc131/junit/GiftCardTest.java
@@ -1,8 +1,9 @@
 package csc131.junit;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThrows;
 import org.junit.Test;
-import org.junit.rules.ExpectedException;
+//import org.junit.rules.ExpectedException;
 
 public class GiftCardTest{
 	
@@ -40,7 +41,7 @@ public class GiftCardTest{
 	}
 	
 	@Test
-	public void deduct() {
+	public void deductInvalidAmount() {
 		
 		double balance;
 		int issuingstore;
@@ -50,6 +51,55 @@ public class GiftCardTest{
 		balance = 100.00;
 		card = new GiftCard(issuingstore, balance);
 		
-		assertEquals("deduct()","Invalid Transaction",card.deduct(-1337));
+		assertEquals("deduct():Invalid Number","Invalid Transaction",card.deduct(-1337));
+	}
+	
+	@Test
+	public void deductValidAmountWCardBalance() {
+		
+		double balance;
+		int issuingstore;
+		GiftCard card;
+		
+		issuingstore = 1337;
+		balance = 100.00;
+		card = new GiftCard(issuingstore, balance);
+		
+		assertEquals("deduct():Invalid Number","Remaining Balance: " + String.format("%6.2f",63.00),card.deduct(37));
+		
+	}
+	
+	@Test
+	public void deductValidAmountWAmountDue() {
+		
+		double balance;
+		int issuingstore;
+		GiftCard card;
+		
+		issuingstore = 1337;
+		balance = 100.00;
+		card = new GiftCard(issuingstore, balance);
+		
+		assertEquals("deduct():Invalid Number","Amount Due: "+ String.format("%6.2f",37.00),card.deduct(137));
+		
+	}
+	
+	@Test
+	public void constructor_Illegal_Balance() {	
+		assertThrows(IllegalArgumentException.class, () -> {new GiftCard(1, -100.00);});
+	}
+	
+	@Test
+	public void constructor_IncorrectID_Low(){
+		assertThrows(IllegalArgumentException.class, () -> {new GiftCard(-1, 100.00);});
+		
+		
+	}
+	
+	@Test
+	public void constructor_IncorrectID_High(){
+
+	    assertThrows(IllegalArgumentException.class, () -> {new GiftCard(10000, 100.00);});
+
 	}
 }
